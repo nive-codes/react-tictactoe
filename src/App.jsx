b707bb7f8ac1034b83d88c20270e29dd25d23de5
nive.codes <nive.codes@gmail.com>
2026-04-14 10:11:20 +0900

    게임보드 로직 -> app.jsx 이전 (state 파생 처리)

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
 import Player from "./components/Player";
 import GameBoard from "./components/GameBoard";
 import { useState } from "react";
+import Log from "./components/Log";
 function App() {
-
+  const [gameTurns, setGameTurns] = useState([]); //게임 턴을 진행할때마다 배열 생성(log row 추가 느낌)
   const [activePlayer, setActivePlayer] = useState('X');
 
-  function handleSelectSquare(){
+  function handleSelectSquare(rowIndex, colIndex){
     setActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X') //기존 사용자 확인
+    setGameTurns(prevTurns => {
+      let currentPlayer = 'X';
+      if(prevTurns.length > 0 && prevTurns[0].player === 'X'){
+        currentPlayer = 'O';
+      }
+
+      const updatedTurns = [{ square: {row: rowIndex, col: colIndex},player: currentPlayer},
+        ...prevTurns];
+
+        return updatedTurns;
+    });
   }
 
   return <main>
@@ -18,10 +30,9 @@ function App() {
       </ol>
 
       {/* gameboard에서 사용할 player의 상태값을 확인하는 것을 gameboard에서 실행*/}
-      <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer}/>  
+      <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns}/>  
     </div>
-
-    LOG
+    <Log/>
   </main>;
 }
 
