c75275f8772a0845debf220cc5271ca4db13ac27
nive.codes <nive.codes@gmail.com>
2026-04-14 11:20:00 +0900

    winner 추가 및 gameboard 로직 app 이관

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,20 @@ import Player from "./components/Player";
 import GameBoard from "./components/GameBoard";
 import { useState } from "react";
 import Log from "./components/Log";
+import { WINNING_COMBINATIONS } from "./components/winning-combinations";
 
-// 상태를 최소한으로 사용하도록 처리
+
+/**
+ * 게임판
+ */
+const initialGameBoard = [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null]
+];
+
+
+// 상태를 최소한으로 사용하도록 처리ㅔ
 // playser state를 삭제하고 사용할 헬퍼 함수 
 function deriveActivePlayer(gameTurns){
   let currentPlayer = 'X';
@@ -14,11 +26,44 @@ function deriveActivePlayer(gameTurns){
   return currentPlayer;
 }
 
+
+
+
 function App() {
   const [gameTurns, setGameTurns] = useState([]); //게임 턴을 진행할때마다 배열 생성(log row 추가 느낌)
-  // const [activePlayer, setActivePlayer] = useState('X');
+  // const [hasWinnter, setHasWinner] = useState(false); //사실 불필요. 매번 update될때마다 gameTurns에 있기 때문.
+  // const [activePlayer, setActivePlayer] = useState('X'); //gameTurns에 포함되어 있으므로 주석
 
   const activePlayer = deriveActivePlayer(gameTurns);
+
+  let gameBoard = initialGameBoard;
+
+  /**
+   * gameboard처리
+   */
+  for(const turn of gameTurns){
+        const {square, player} = turn;    //squre와 turn을 꺼냄
+        const { row, col } = square;  //suqre안에서 row와 col을 꺼냄
+        
+        gameBoard[row][col] = player;
+  }
+
+  let winner = null;
+
+  //우승 검토 처리
+  for(const combination of WINNING_COMBINATIONS){
+    const firstSquareSymbol = gameBoard[combination[0].row][combination[0].column];
+    const secondSquareSymbol = gameBoard[combination[1].row][combination[2].column]
+    const thirdSquareSymbol= gameBoard[combination[2].row][combination[1].column];
+
+    if(firstSquareSymbol
+      && firstSquareSymbol === secondSquareSymbol 
+      && firstSquareSymbol === thirdSquareSymbol){
+          
+    }{
+      winner = firstSquareSymbol;
+    }
+  }
 
 
   function handleSelectSquare(rowIndex, colIndex){
@@ -42,8 +87,10 @@ function App() {
         <Player initialName="Player 2" symbole="O" isActive={activePlayer === 'O'}/>
       </ol>
 
+      
+      {winner && <p>You Won, {winner}!</p>}
       {/* gameboard에서 사용할 player의 상태값을 확인하는 것을 gameboard에서 실행*/}
-      <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns}/>  
+      <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard}/>  
     </div>
     <Log turns={gameTurns}/>
   </main>;
--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,37 +1,12 @@
 
-const initialGameBoard = [
-    [null, null, null],
-    [null, null, null],
-    [null, null, null]
-]
+export default function GameBoard({onSelectSquare, board}){
 
-export default function GameBoard({onSelectSquare, turns}){
-    let gameBoard = initialGameBoard;
 
-    for(const turn of turns){
-          const {square, player} = turn;    //squre와 turn을 꺼냄
-          const { row, col } = square;  //suqre안에서 row와 col을 꺼냄
-          
-          gameBoard[row][col] = player;
-    }
-
-    // gameboard가 아닌 app.jsx에서 제어하도록. app.jsx에서 상태를 알아야 board, log 핸들링이 가능함.
-    // const [gameBoard, setGameBoard] = useState(initialGameBoard);
-
-    // function handleSelectSquare(rowIndex, colIndex){
-    //     setGameBoard((prevGameBoard) => {
-    //         const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])];    //메모리에 저장되는 새로운 게임보드로, prevGameBoard의 정보를 모두 가지고 있음.
-    //         updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
-    //         return updatedBoard;
-    //     }); //이전 상태를 가진 채로 이동
-
-    //     onSelectSquare();
-    // }
 
     return <ol id="game-board">
         {/* gameBoard로 해야 변경된 상태값이 계속해서 유지가 되고 있음 */}
         {/* {initialGameBoard.map((row, rowIndex) => <li key={rowIndex}> */}
-        {gameBoard.map((row, rowIndex) => <li key={rowIndex}>       
+        {board.map((row, rowIndex) => <li key={rowIndex}>       
             <ol>
                 {row.map((playerSymbol, colIndex) =>
                      <li key={colIndex}>
